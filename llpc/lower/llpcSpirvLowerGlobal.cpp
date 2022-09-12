91c0d32d7e14b1d83b9dba51a9e8adf061fdb94c
Mariusz Sikora <mariusz.sikora@amd.com>
2022-09-12 11:00:24 +0200

    Opaque pointers: Fix lowerBufferBlock

--- llpc/lower/llpcSpirvLowerGlobal.cpp
+++ llpc/lower/llpcSpirvLowerGlobal.cpp
@@ -1762,6 +1762,7 @@ void SpirvLowerGlobal::lowerBufferBlock() {
     BitCastInst *bitCastInst;                         // The user is a bitCast
     SelectInst *selectInst;                           // The user is a select
     SmallVector<GetElementPtrInst *> getElemPtrInsts; // The user is a GEP. If the user is a select, we store its users.
+    Value *loadStoreInst;
   };
 
   // Skip the globals that are handled with previous global.
@@ -1824,6 +1825,10 @@ void SpirvLowerGlobal::lowerBufferBlock() {
               // We need to modify the bitcast if we did not find a GEP.
               assert(bitCast->getOperand(0) == &global);
               replaceInstsInfo.bitCastInst = bitCast;
+            } else if (auto *load = dyn_cast<LoadInst>(inst)) {
+              replaceInstsInfo.loadStoreInst = load;
+            } else if (auto *store = dyn_cast<StoreInst>(inst)) {
+              replaceInstsInfo.loadStoreInst = store;
             } else {
               // The users of the select must be a GEP.
               SelectInst *selectInst = cast<SelectInst>(inst);
@@ -1856,6 +1861,17 @@ void SpirvLowerGlobal::lowerBufferBlock() {
               m_builder->CreateInvariantStart(bufferDesc);
 
             replaceInstsInfo.bitCastInst->replaceUsesOfWith(&global, m_builder->CreateBitCast(bufferDesc, blockType));
+          } else if (replaceInstsInfo.loadStoreInst) {
+            m_builder->SetInsertPoint(cast<Instruction>(replaceInstsInfo.loadStoreInst));
+
+            Value *const bufferDesc = m_builder->CreateLoadBufferDesc(
+                descSet, binding, m_builder->getInt32(0), global.isConstant() ? 0 : lgc::Builder::BufferFlagWritten,
+                m_builder->getInt8Ty());
+
+            if (global.isConstant())
+              m_builder->CreateInvariantStart(bufferDesc);
+
+            cast<Instruction>(replaceInstsInfo.loadStoreInst)->replaceUsesOfWith(&global, bufferDesc);
           } else {
             assert(!replaceInstsInfo.getElemPtrInsts.empty());
 
